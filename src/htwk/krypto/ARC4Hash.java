bfa2839337751f2b56f0e72ffb16b35e263edf53
KaneLebt <KaneLebt@trashmail.de>
2015-01-12 11:57:20 +0100

    add sr4hash funktion

--- src/htwk/krypto/ARC4Hash.java
+++ src/htwk/krypto/ARC4Hash.java
@@ -1,10 +1,13 @@
 package htwk.krypto;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ARC4Hash{
  
         public static void main (String[] args){
             String k = DateiAuslessen();
-
+            
             int temp = Blockzerlegung(k);
             int text = initSHA256();
 
@@ -22,7 +25,7 @@ public class ARC4Hash{
 
         public static String DateiAuslessen(){
             System.out.println("\n1. Der aus einer Datei eingelesene Klartext wird wie in SHA-256 aufgefüllt.");
-            return "Hello World";        
+            return "Hello World! usw";        
         }
 
         public static int Blockzerlegung( String k){
@@ -53,5 +56,39 @@ public class ARC4Hash{
         public static int nextARC4(int arc){
             System.out.println("\n8. Die nächsten ausgegebenen 256 Bit des ARC4 liefern den neuen Inhalt des Textregisters.");
             return arc;
+        }
+        
+        public static String SHAHashing (String password){
+        	
+        	MessageDigest md;
+			try {
+				md = MessageDigest.getInstance("SHA-256");
+			
+            md.update(password.getBytes());
+     
+            byte byteData[] = md.digest();
+     
+            //convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+     
+            System.out.println("Hex format : " + sb.toString());
+     
+            //convert the byte to hex format method 2
+            StringBuffer hexString = new StringBuffer();
+        	for (int i=0;i<byteData.length;i++) {
+        		String hex=Integer.toHexString(0xff & byteData[i]);
+       	     	if(hex.length()==1) hexString.append('0');
+       	     	hexString.append(hex);
+        	}
+        	System.out.println("Hex format : " + hexString.toString());
+        	return hexString.toString();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return password;
+			}
         }
 }
